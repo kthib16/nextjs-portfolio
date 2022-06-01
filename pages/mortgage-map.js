c3a484a3f9ebe3c3196254d96571e74ad7ba23b8
Kevin Thibodeaux <kthib16@gmail.com>
2022-06-01 09:22:29 -0400

    updates index

--- pages/mortgage-map.js
+++ pages/mortgage-map.js
@@ -12,7 +12,7 @@ return(
     <p>As part of a retrospective event on the 10-year anniversary of the financial crisis, the Economic Studies program at Brookings produced a number of research looking into the causes of the crisis. I created an <a href="https://www.brookings.edu/blog/up-front/2018/09/10/mapping-the-boom-in-nonbank-mortgage-lending-and-understanding-the-risks/" className='text-purple-300 hover:text-purple-500'>interactive map</a> using D3.js to accompany one such piece. The map shows where the share of nonbank mortgages are greatest in the U.S. On hover over a county, a tool tip displays both the percentage of all mortgages as well as the county name.</p>
     <div className="mt-4 mb-4">
     <Image
-      priority
+      priority="true"
         src='/images/mortgage-market-map.png'
         width='799'
         height = '544' />
